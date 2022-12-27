c177c93c266e995a84611fa34d7e26d1b867a941
ER1KO <enricovagnetti@gmail.com>
2022-12-27 16:02:55 +0100

    Primo commit

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/imv2myq.css"></link>
+        <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
