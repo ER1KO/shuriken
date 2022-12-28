4b793ff4a2c47b32cd33e28a06c0c53aab82d226
ER1KO <enricovagnetti@gmail.com>
2022-12-28 16:11:36 +0100

    Probabile fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/imv2myq.css"></link>
-        <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+        <Script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></Script>
       </Head>
       <body className='bg-[#010102]'>
         <Main />
